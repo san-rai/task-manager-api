222d981b8ddddd2745d210bd2b8263749125c18d
Sandesh Rai <s.rai7@outlook.com>
2024-03-12 14:43:35 +0000

    Remove unused imports

--- src/main/java/sandeshrai/taskmanager/api/repository/TasksRepository.java
+++ src/main/java/sandeshrai/taskmanager/api/repository/TasksRepository.java
@@ -3,9 +3,4 @@ package sandeshrai.taskmanager.api.repository;
 import org.springframework.data.repository.ListCrudRepository;
 import sandeshrai.taskmanager.api.model.Task;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
 public interface TasksRepository extends ListCrudRepository<Task,Long> { }
